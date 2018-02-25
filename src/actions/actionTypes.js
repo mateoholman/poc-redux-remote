63e2330fead4dcfb74500644b9ea8b2532c70dff
Matthew Holman <mateoholman@gmail.com>
2018-02-25 13:20:08 -0600

    Moved actionTypes constants

--- src/actions/actionTypes.js
+++
@@ -1,3 +0,0 @@
-// This file should only contain the action type constants
-// Action types should be in the following format:
-// const BOOK_SELECTED = 'BOOK_SELECTED';
---
+++ src/constants/actionTypes.js
@@ -0,0 +1,2 @@
+export const SUBMIT_FORM = 'SUBMIT_FORM';
+export const UPDATE_FORM = 'UPDATE_FORM';
