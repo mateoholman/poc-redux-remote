59445abef2dff36b2b3d681f6e5797f651bb6cc0
Matthew Holman <mateoholman@gmail.com>
2018-02-27 21:38:08 -0600

    Added redux devtools extension

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
 //Add any middlewear to the HOC below
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(),
+  // other store enhancers if any
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.getElementById('root'));
